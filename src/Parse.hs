a9d11768afeb054885222ec737a14f772fc1e49f
Tobin Yehle <tobinyehle@gmail.com>
2020-10-19 21:39:55 -0600

    improve parsing error handling

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,7 +1,9 @@
-module Parse (parse) where
+module Parse where
 
 import Data.Bifunctor (first)
 import Data.List (isPrefixOf)
+import Data.Map (Map)
+import qualified Data.Map as Map
 import qualified Text.Megaparsec as M (errorBundlePretty, parse)
 
 import ParseLisp
@@ -15,29 +17,54 @@ parse filename = first M.errorBundlePretty . M.parse parser filename
 
 
 translate :: MonadFail m => Lisp -> m Expr
-translate sym@(Symbol _) =
-  Var <$> parseIdentifier sym
-translate (String s) = fail $ "Unexpected string" ++ show s
-translate (Number n) = return $ NumExp n
-translate (Float f) = fail $ "Unexpected float" ++ show f
-translate (List []) = fail "Unexpected nil"
-translate (List [Symbol "lambda", List args, body]) =
-  Lam <$> mapM parseIdentifier args <*> translate body
-translate (List [Symbol "let", List [name, value], body]) =
-  Let <$> parseIdentifier name <*> translate value <*> translate body
-translate (List [Symbol "letrec", List [name, value], body]) =
-  Letrec <$> parseIdentifier name <*> translate value <*> translate body
-translate (List [Symbol "if0", c, t, f]) =
-  If0 <$> translate c <*> translate t <*> translate f
-translate (List (fn : args)) =
-  App <$> translate fn <*> mapM translate args
+translate expr = case expr of
+  -- atoms
+  Symbol _ -> Var <$> parseIdentifier expr
+  String s -> fail $ "Unexpected string" ++ show s
+  Number n -> return $ NumExp n
+  Float f -> fail $ "Unexpected float" ++ show f
+  List [] -> fail "Unexpected nil"
+  -- keywords
+  List (Symbol name : exprs) -> parseKeyword name exprs
+  -- assume application if the list does not start with a keyword
+  List (fn : args) -> App <$> translate fn <*> mapM translate args
 
 
-keywords :: [String]
-keywords = ["let", "letrec", "lambda", "if0"]
+parseKeyword :: MonadFail m => String -> [Lisp] -> m Expr
+parseKeyword name args = case Map.lookup name keywordParsingTable of
+  Nothing -> App <$> translate (Symbol name) <*> mapM translate args
+  Just parseFunction -> parseFunction args
+
+
+keywordParsingTable :: MonadFail m => Map String ([Lisp] -> m Expr)
+keywordParsingTable = Map.fromList
+  [ ("lambda", \exprs -> case exprs of
+      [List args, body] -> Lam <$> mapM parseIdentifier args <*> translate body
+      _ -> syntaxFailure "lambda"
+    )
+  , ("let", \exprs -> case exprs of
+      [List [name, value], body] -> Let <$> parseIdentifier name <*> translate value <*> translate body
+      _ -> syntaxFailure "let"
+    )
+  , ("letrec", \exprs -> case exprs of
+      [List [name, value], body] -> Letrec <$> parseIdentifier name <*> translate value <*> translate body
+      _ -> syntaxFailure "letrec"
+    )
+  , ("if0", \exprs -> case exprs of
+      [c, t, f] -> If0 <$> translate c <*> translate t <*> translate f
+      _ -> syntaxFailure "if0"
+    )
+  ]
+  where
+    syntaxFailure :: MonadFail m => String -> m a
+    syntaxFailure name = fail $ "Invalid syntax in " ++ name ++ " expression"
+
 
 parseIdentifier :: MonadFail m => Lisp -> m VarIdent
-parseIdentifier (Symbol name)
-  | name `elem` keywords || isPrefixOf "__" name = fail $ "Invalid identifier: " ++ name
-  | otherwise = return $ VarIdent name
-parseIdentifier bad = fail $ "Invalid identifier: " ++ show bad
+parseIdentifier expr = case expr of
+  (Symbol name) | isKeyword name || isInvalid name -> fail $ "Invalid identifier: " ++ name
+  (Symbol name) -> return $ VarIdent name
+  bad -> fail $ "Invalid identifier: " ++ show bad
+  where
+    isKeyword name = Map.member name (keywordParsingTable :: Map String ([Lisp] -> Maybe Expr))
+    isInvalid name = isPrefixOf "__" name
